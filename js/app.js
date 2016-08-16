92ecd2bbe7ccfee65252fa3d564a54e32c5603a6
cahndrew <cahndrew@gmail.com>
2016-08-16 16:03:02 +0300

    content slider

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 $(document).foundation();
 new mlPushMenu( document.getElementById( 'mp-menu' ), document.getElementById( 'trigger' ) )
+
+  var owl = $("#owl-demo");
+
+  owl.owlCarousel({
+    navigation : true,
+    singleItem : true,
+    transitionStyle : "fade"
+  });
+
+});
